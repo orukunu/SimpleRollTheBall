5a931e8b123cd5964b6e8edd98f24ed4f3c92387
Şükrü Orkun ÇELİK <s.orkuncelik@gmail.com>
2020-07-27 17:38:10 +0300

    Deploy

---
+++ Assets/Codes/MyBallHandling.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyBallHandling : MonoBehaviour
+{
+    Rigidbody physics;
+    public int speed;
+    int score = 0;
+    public int collectionSize;
+    public Text scoreText;
+    public Text timeText;
+    public Text gameOverText;
+    float totalTime = Time.fixedTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalRaw = Input.GetAxisRaw("Horizontal");
+        float verticalRaw = Input.GetAxisRaw("Vertical");
+        timeText.text = "TIME :" + Time.fixedTime;
+
+        Vector3 velocityVector = new Vector3(horizontalRaw,0,verticalRaw);
+
+        physics.AddForce(velocityVector*speed);
+    }
+
+    void OnTriggerEnter(Collider obsticale)
+    {
+        if (obsticale.gameObject.tag == "Obsticale")
+        {
+            obsticale.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "SCORE = " + score;
+
+            if ( score == collectionSize)
+            {
+                gameOverText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Codes/MyCameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraFollower : MonoBehaviour
+{
+    public GameObject ballToFollow;
+    Vector3 distanceBetween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceBetween = transform.position - ballToFollow.transform.position;        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ballToFollow.transform.position + distanceBetween;
+    }
+}
